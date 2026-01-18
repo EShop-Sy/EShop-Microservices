d4882da56cf94ad340c7e8644ecd74df98be52fe
Fayez Saadi <fayezosaadi@proton.me>
2026-01-17 23:52:15 -0700

    feat(api): add basket features

--- src/Aspire/AppHost/AppHost.cs
+++ src/Aspire/AppHost/AppHost.cs
@@ -31,7 +31,7 @@ var keycloak = builder.AddKeycloak("keycloak-auth-service")
     .WithArgs("--http-enabled=true")
     .WithArgs("--hostname=localhost")
     .WithOtlpExporter()
-    .WithHttpsEndpoint()
+    // .WithHttpsEndpoint()
     .WithIconName("KeyMultiple");
 
 // #pragma warning disable ASPIRECERTIFICATES001
