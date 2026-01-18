38da5f022311fe4897ee8451adce1e317752f302
Fayez Saadi <fayezosaadi@proton.me>
2026-01-17 17:25:48 -0700

    feat(api): add basket features

--- src/Aspire/AppHost/AppHost.cs
+++ src/Aspire/AppHost/AppHost.cs
@@ -27,7 +27,7 @@ var secret = builder.AddParameter("KeycloakClientSecret", secret: true);
 var keycloak = builder.AddKeycloak("keycloak")
     .WithRealmImport("./realms")
     .WithEnvironment("CLIENT_SECRET", secret)
-    .WithArgs("http-enabled", true)
+    .WithArgs("KC_HTTP_ENABLED", true)
     .WithOtlpExporter()
     .WithIconName("KeyMultiple");
 
