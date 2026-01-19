d700c4fdbb9a4afbbfeb583d28438c7a287c6870
Fayez Saadi <fayezosaadi@proton.me>
2026-01-19 14:21:46 -0700

    feat(api): add basket features

--- src/Aspire/AppHost/AppHost.cs
+++ src/Aspire/AppHost/AppHost.cs
@@ -28,7 +28,7 @@ var keycloak = builder.AddKeycloak("keycloak-service", 8080)
     .WithRealmImport("./realms")
     .WithArgs("--http-enabled=true")
     .WithArgs("--proxy-headers=forwarded")
-    .WithArgs("--hostname=https://keycloak-service.internal")
+    .WithArgs("--hostname=https://keycloak-service.internal.jollyground-0d1a883d.uaenorth.azurecontainerapps.io")
     .WithEnvironment("CLIENT_SECRET", secret)
     .WithIconName("KeyMultiple");
 
