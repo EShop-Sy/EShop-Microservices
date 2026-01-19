d00fdb098d13ff9a38c37f03f6fc64d1498cf35e
Fayez Saadi <fayezosaadi@proton.me>
2026-01-18 22:45:30 -0700

    feat(api): add basket features

--- src/Aspire/AppHost/Extensions/YarpResourceBuilderExtensions.cs
+++ src/Aspire/AppHost/Extensions/YarpResourceBuilderExtensions.cs
@@ -17,25 +17,7 @@ internal static class YarpResourceBuilderExtensions
                 yarp.AddRoute($"/{name}/{{**catch-all}}", cluster)
                     .WithTransformPathRouteValues("/{**catch-all}")
                     .WithTransformForwarded(useHost: true, useProto: true, forFormat: NodeFormat.IpAndPort,
-                        byFormat: NodeFormat.Random, action: ForwardedTransformActions.Set)
-                    // .WithTransformForwarded()
-                    // .WithTransformForwarded(useHost: true, useProto: true, forFormat: NodeFormat.IpAndPort, byFormat: NodeFormat.Random, action: ForwardedTransformActions.Append)
-                    // .WithTransformRequestHeader("X-Forwarded-Host", "gateway.eshop.sy.com")
-                    // .WithTransformRequestHeader("RequestHeader", "X-Forwarded-Proto")
-                    // .WithTransformRequestHeader("Set", "https")
-                    // .WithTransformXForwarded(
-                    //     //     // headerPrefix: ForwardedTransformActions.Remove,
-                    //     // xDefault: ForwardedTransformActions.Set,
-                    //     // xHost:
-                    //     xFor: ForwardedTransformActions.Append,
-                    //     xHost: ForwardedTransformActions.Append,
-                    //     xProto: ForwardedTransformActions.Append
-                    //     //     xPrefix: ForwardedTransformActions.Remove
-                    //     //     // ForwardedTransformActions? xFor = null,
-                    //     //     // ForwardedTransformActions? xHost = null,
-                    //     //     // ForwardedTransformActions? xProto = null,
-                    //     //     // ForwardedTransformActions? xPrefix = null
-                    // )
+                        byFormat: NodeFormat.Random, action: ForwardedTransformActions.Append)
                     .WithTransformResponseHeader("X-Powered-By", "YARP");
             }
         });
